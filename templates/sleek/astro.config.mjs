d46ee30434a81e4edf019a91fbc455b2889ea402
maranief-boop <maranief@gmail.com>
2026-03-27 09:45:54 -0300

    Update astro.config.mjs

--- templates/sleek/astro.config.mjs
+++ templates/sleek/astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://hiitdelas.com.br",
   integrations: [
-    sitemap(),
     tailwind({
       config: {
         applyBaseStyles: false,
