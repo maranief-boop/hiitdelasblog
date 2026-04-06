0c6c83ef16c47e4b5b45466c56bf2d9f7c283b5d
maranief-boop <maranief@gmail.com>
2026-04-06 07:33:36 -0300

    Update astro.config.mjs

--- templates/sleek/astro.config.mjs
+++ templates/sleek/astro.config.mjs
@@ -1,17 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
 
+// Voltamos ao básico para o site subir agora
 export default defineConfig({
   site: "https://hiitdelas.com.br",
-  // Adicionamos o outDir explicitamente como relativo para evitar o erro do deploy
-  outDir: "./dist", 
   integrations: [
     tailwind({
       config: {
         applyBaseStyles: false,
       },
     }),
-    sitemap(),
   ],
 });
